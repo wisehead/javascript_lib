4e97d4af6dab164230a3db580d0ef0705ef73f33
Hui Chen <alex.chenhui@gmail.com>
2026-01-11 20:47:10 +0800

    edit Row works

--- 06.Table_IUD/table.js
+++ 06.Table_IUD/table.js
@@ -17,12 +17,7 @@ function addRow() {
     //修改节点文本内容
     nameCol.innerHTML = '未命名';
     phoneCol.innerHTML = "无联系方式";
-    actionCol.innerHTML = '<button>编辑</button><button onclick="deleteRow(this)">删除</button>'
-}
-
-// 编辑数据函数
-function editRow(button) {
-    console.log(button);
+    actionCol.innerHTML = '<button onclick="editRow(this)">编辑</button><button onclick="deleteRow(this)">删除</button>'
 }
 
 //删除数据函数
@@ -31,4 +26,15 @@ function deleteRow(button) {
     var row = button.parentNode.parentNode;
     console.log(row);
     row.parentNode.removeChild(row);
+}
+
+//编辑数据函数
+function editRow(button) {
+    var row = button.parentNode.parentNode;
+    var name = row.cells[0];
+    var phone = row.cells[1];
+    var inputName = prompt("请输入名字:");
+    var inputPhone = prompt("请输入联系方式");
+    name.innerHTML = inputName;
+    phone.innerHTML = inputPhone;
 }
