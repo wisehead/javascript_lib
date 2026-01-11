e1db93901b514b7232e2a8e09085d9a0c4b6a85a
Hui Chen <alex.chenhui@gmail.com>
2026-01-11 20:21:08 +0800

    addRow works

---
+++ 06.Table_IUD/table.js
@@ -0,0 +1,31 @@
+//新增数据函数
+function addRow() {
+    var table = document.getElementById('table');
+    // console.log(table);
+    //获取插入的位置
+
+    var length = table.rows.length;
+    // console.log(length);
+    //插入行节点
+    var newRow = table.insertRow(length);
+    // console.log(newROW);
+    //插入列节点对象
+    var nameCol = newRow.insertCell(0);
+    var phoneCol = newRow.insertcell(1);
+    var actionCol = newRow.insertcell(2);
+    //修改节点文本内容
+    //修改节点文本内容
+    nameCol.innerHTML = '未命名';
+    phoneCol.innerHTML = "无联系方式";
+    actionCol.innerHTML = '<button>编辑</button><button>删除</button>'
+}
+
+// 编辑数据函数
+function editRow(button) {
+    console.log(button);
+}
+
+// 删除数据函数
+function deleteRow(button) {
+    console.log(button);
+}
