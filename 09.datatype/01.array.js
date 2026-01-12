c704398a9c2a2934b09bb2324be297a61b055e2f
Hui Chen <alex.chenhui@gmail.com>
2026-01-12 10:15:54 +0800

    array

---
+++ 09.datatype/01.array.js
@@ -0,0 +1,17 @@
+var cars = new Array();
+cars[0] = "Saab";
+cars[1] = "Volvo";
+cars[2] = "BMW";
+console.log(cars);
+
+var cars = new Array("Saab", "Volvo", "BMW");
+console.log(cars2);
+
+var cars3 = ["Saab", "Volvo", "BMW"];
+console.log(cars3);
+
+var cars4 = new Array(3);
+cars4[0] = "Saab";
+cars4[1] = "Volvo";
+cars4[2] = "BMW";
+console.log(cars4);
