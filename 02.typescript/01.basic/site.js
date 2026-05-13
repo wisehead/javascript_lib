d5e9643fd2e3e47a9bb6b30a791754485f88d4f6
Hui Chen <alex.chenhui@gmail.com>
2026-05-13 20:46:47 +0800

    site.ts

---
+++ 02.typescript/01.basic/site.js
@@ -0,0 +1,8 @@
+"use strict";
+class Site {
+    name() {
+        console.log("Runoob");
+    }
+}
+var obj = new Site();
+obj.name();
---
+++ 02.typescript/01.basic/site.ts
@@ -0,0 +1,7 @@
+class Site { 
+   name():void { 
+      console.log("Runoob") 
+   } 
+} 
+var obj = new Site(); 
+obj.name();
