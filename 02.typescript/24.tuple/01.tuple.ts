9b2eb53f2a65d0aac5a2cd5a5d85160473085c27
Hui Chen <alex.chenhui@gmail.com>
2026-05-21 16:28:48 +0800

    tuple

---
+++ 02.typescript/24.tuple/01.tuple.ts
@@ -0,0 +1,10 @@
+let mytuple: [number, string, boolean] = [42, "Runoob", true];
+ 
+// 访问元组中的元素
+let num = mytuple[0]; // 访问第一个元素，值为 42，类型为 number
+let str = mytuple[1]; // 访问第二个元素，值为 "Runoob"，类型为 string
+let bool = mytuple[2]; // 访问第三个元素，值为 true，类型为 boolean
+ 
+console.log(num);  // 输出: 42
+console.log(str);  // 输出: Runoob
+console.log(bool); // 输出: true
