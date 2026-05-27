12514f509f238d1e9bb07fe8523b4bb0abe1fb1d
Hui Chen <alex.chenhui@gmail.com>
2026-05-27 18:35:35 +0800

    works

---
+++ 02.typescript/57.project/build.sh
@@ -0,0 +1,12 @@
+#############################################################
+#   File Name: build.sh
+#     Autohor: Hui Chen (c) 2026
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2026/05/27-18:34:13
+#############################################################
+#!/bin/sh 
+#cd /Users/wisehead/github/javascript_lib/02.typescript/57.project && 
+npx vite --port 3001 2>&1 &; 
+#sleep 3 && curl -s -o /dev/null -w "%{http_code}" http://localhost:3001/
+
+#npm run dev
