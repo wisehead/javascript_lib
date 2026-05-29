6fd90d0e7b246091791d060cc193107f659cda8e
Hui Chen <alex.chenhui@gmail.com>
2026-05-29 18:53:07 +0800

    vue3

---
+++ 02.typescript/59.vue3/init.sh
@@ -0,0 +1,15 @@
+#############################################################
+#   File Name: init.sh
+#     Autohor: Hui Chen (c) 2026
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2026/05/29-17:29:10
+#############################################################
+#!/bin/sh 
+# 使用 Vite 创建 Vue 3 + TypeScript 项目（推荐）
+npm create vite@latest my-vue-app -- --template vue-ts
+
+# 进入项目目录
+cd my-vue-app
+
+# 安装依赖
+npm install
