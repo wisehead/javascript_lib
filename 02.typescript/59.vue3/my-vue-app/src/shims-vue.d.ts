d4b410f80f4815ab94668120a99a83605d7e0e2f
Hui Chen <alex.chenhui@gmail.com>
2026-05-30 19:52:14 +0800

    add *.gif

--- 02.typescript/59.vue3/my-vue-app/src/shims-vue.d.ts
+++ 02.typescript/59.vue3/my-vue-app/src/shims-vue.d.ts
@@ -7,3 +7,23 @@ declare module "*.png" {
   const content: string;
   export default content;
 }
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: string;
+  export default content;
+}
