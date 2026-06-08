793874ab98da29238d7b78d21ade77842ece180a
Hui Chen <alex.chenhui@gmail.com>
2026-06-08 20:47:58 +0800

    style

---
+++ 03.react/04.jsx/05.style.jsx
@@ -0,0 +1,8 @@
+var myStyle = {
+    fontSize: 100,
+    color: '#FF0000'
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <h1 style = {myStyle}>菜鸟教程</h1>
+);
