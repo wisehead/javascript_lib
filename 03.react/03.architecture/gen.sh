76ca307e68307eb3fe4664c8ee24d2a304866488
Hui Chen <alex.chenhui@gmail.com>
2026-06-08 19:52:53 +0800

    arch

---
+++ 03.react/03.architecture/gen.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: gen.sh
+#     Autohor: Hui Chen (c) 2026
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2026/06/08-19:41:58
+#############################################################
+#!/bin/sh 
+pnpm create vite@latest my-first-react-app -- --template react
